d3244da80446dba60461932385f6f6b61fb4a7fe
Wild_Mike_wzy <Wild_Mike_wzy@outlook.com>
2026-04-12 16:58:14 +0800

    修改了Robot.cpp文件和Robot.h文件，实现了机器人的实时运动。

--- source_file/Robot.cpp
+++ source_file/Robot.cpp
@@ -1,20 +1,46 @@
 /*机器人动态逻辑接口*/
 #include "Robot.h"
 
-Robot::Robot(int _id, Point start) : id(_id), currentPos(start), status(RobotStatus::IDLE) {}   //构造函数，初始化机器人的状态和位置
+Robot::Robot(int _id, Point start)
+    : id(_id), currentPos(start), status(RobotStatus::IDLE) {
+    realX = start.x;
+    realY = start.y;
+}   //构造函数，初始化机器人的状态和位置
 
-void Robot::setPath(const std::vector<Point>& newPath) {            // 接收 Router 给的路径，更新状态
+void Robot::setPath(const std::vector<Point>& newPath) {      // 接收 Router 给的路径，更新状态
     pathQueue = newPath;
     if (!pathQueue.empty()) status = RobotStatus::MOVING;
 }
 
-void Robot::update() {          //每帧调用一次，处理移动逻辑
+void Robot::update() {
     if (status == RobotStatus::MOVING && !pathQueue.empty()) {
-        // 简单的逻辑：每调用一次，取路径中的下一个点
-        // 逻辑控制员-李忱刚任务：在这里需要实现平滑移动动画，而不是瞬间跳格（下面的代码是直接跳格的临时逻辑）
-        currentPos = pathQueue.front();
-        pathQueue.erase(pathQueue.begin());
+        Point target = pathQueue.front();
 
+        // 计算当前位置与目标格点像素中心点的偏移，开始移动（先X后Y)
+        float step = 0.1f;//移动长度
+
+        if (abs(realX - target.x) > 0.01f) {
+            if (realX < target.x) realX += step;
+            else if (realX > target.x) realX -= step;
+        }
+
+        else if (abs(realY - target.y) > 0.01f) {
+            if (realY < target.y) realY += step;
+            else if (realY > target.y) realY -= step;
+        }
+
+        currentPos.x = (int)round(realX);
+        currentPos.y = (int)round(realY);
+
+        // 判定是否“足够接近”目标点（防止浮点数抖动）
+        if (abs(realX - target.x) < 0.12f && abs(realY - target.y) < 0.12f) {
+            realX = target.x;
+            realY = target.y;
+            currentPos = target;    // 强行对齐到格点
+            pathQueue.erase(pathQueue.begin()); // 前往路径序列的下一个点
+        }
+
+        // 到达终点判定-空闲
         if (pathQueue.empty()) status = RobotStatus::IDLE;
     }
 }
